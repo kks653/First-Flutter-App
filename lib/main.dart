a080305306ba2cbd589cf4b3174f71bfde012eb3
KYUNG KOOK SON <kyungkook.son@gmail.com>
2022-03-12 17:28:03 +0900

    feat: adding profile images on top of home

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,10 @@ class Grade extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/flying.gif"),
+              radius: 60.0,
+            ),
             Text("NAME",
               style: TextStyle(
                 color: Colors.white,
@@ -44,7 +48,69 @@ class Grade extends StatelessWidget {
                 fontSize: 28.0,
                 fontWeight: FontWeight.bold
               ),
-            )
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text("BBANTO POWER LEVEL",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("14",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("using lightsaber",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0,
+                ),)
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("face hero tattoo",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),)
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("fire flames",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),)
+              ],
+            ),
 
           ],
         )
